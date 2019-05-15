bbd07e239c3359156ad977f1f3b9042daf507e63
emanuele <emanuele.biotti@gmail.com>
2019-05-15 18:47:38 +0200

    Aggiunta funzione toLowerCase

--- boolzapp.js
+++ boolzapp.js
@@ -52,7 +52,7 @@ $('input').keypress(function(event){
     var nome_ricerca = $('input').val();
     if (nome_ricerca.length > 0){
       $('.contactname h4').each(function(){
-        if($(this).text() === nome_ricerca) {
+        if($(this).text().toLowerCase() === nome_ricerca.toLowerCase()) {
           alert('trovato!');
         }
       })
