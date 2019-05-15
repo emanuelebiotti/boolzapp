ce37d24dedb1255010d82be8457a9c4e26e155e7
emanuele <emanuele.biotti@gmail.com>
2019-05-15 14:22:48 +0200

    aggiunta opzione di risposta altro utente quando si clicca sul microfono

--- boolzapp.js
+++ boolzapp.js
@@ -28,3 +28,20 @@ $('textarea').keypress(function(event){
     }
   }
 });
+
+// ora voglio che esca il testo di risposta dell'altro utente quando si preme sul microfono:
+
+$('.audio').click(function(){
+    //prendo il valore che c'è dentro la textarea, cioé il testo digitato dall'utente
+    var nuovo_messaggio = $('textarea').val();
+    if (nuovo_messaggio.length > 0) {
+    // vado a clonare il template dei messaggi
+      var new_message = $('.message_container.template .message').clone();
+      // aggiungo al contenuto del div il testo digitato dall'utente, e gli aggiungo la classe mine
+      new_message.html(nuovo_messaggio).addClass('yours');
+      // aggiungi tutto il div con dentro il testo inserito dall'utente
+      $('.chat').append(new_message);
+    }  else {
+      alert('testo vuoto!');
+    }
+});
